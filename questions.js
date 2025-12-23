f61f300f057988ed0c1a6743f95a239e82a65184
SmartyLP <smarty100@freenet.de>
2025-12-23 17:35:50 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,15 @@
+const questions = [
+  {
+    id: 1,
+    text: "Wie heißt die Stadt aus Dying Light 2?",
+    options: ["Harran", "Villedor", "Old Town", "Rais City"],
+    correct: 1
+  },
+  {
+    id: 2,
+    text: "Wie heißt der Protagonist?",
+    options: ["Kyle", "Aiden", "Crane", "Spike"],
+    correct: 1
+  }
+];
 
